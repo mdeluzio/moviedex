6001bb1336bedf83aa2efc8a513d6966f5e9f857
Mark DeLuzio <deluziomark@gmail.com>
2019-07-28 11:39:26 -0400

    Refactored for deployment

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const MOVIEBANK = require('./movies.json');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'dev';
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
@@ -77,8 +78,16 @@ function handleGetMovies(req, res) {
 
 app.get('/movie', handleGetMovies)
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+    let response;
+    if(process.env.NODE_ENV === 'production'){
+        response = { error: { message: 'server error' }}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response);
+})
 
-app.listen(PORT, () => (
-    console.log(`Server is listening at http://localhost:${PORT}`)
-))
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {});
